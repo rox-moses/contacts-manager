04ad88a40b7fe68c1260178711689545c8ef82a9
Moses Franco <mosesf@outlook.com>
2017-05-25 14:19:46 -0500

    Created menu, and currently read all contacts from file

--- src/ContactsManager.java
+++ src/ContactsManager.java
@@ -2,6 +2,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created by Moses Franco and Roxana Pupo on 5/25/17
@@ -24,5 +26,27 @@ public class ContactsManager {
 			Files.createFile(dataFile);
 		}
 
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("" +
+				"1. View contacts.\n" +
+				"2. Add a new contact.\n" +
+				"3. Search a contact by name.\n" +
+				"4. Delete an existing contact.\n" +
+				"5. Exit.\n" +
+				"Enter an option (1, 2, 3, 4 or 5):");
+
+
+		int menuOption = scanner.nextInt();
+
+		switch (menuOption) {
+			case 1:
+				List<String> contacts = Files.readAllLines(dataFile);
+				for (String contact : contacts) {
+					System.out.println(contact);
+				}
+
+		}
 	}
+
 }
